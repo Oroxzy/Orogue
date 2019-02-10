ca0fa7e4ad1fa0f9455f35a66064b24d3848f1a4
jeremymeile <jeremymeile@bluewin.ch>
2019-02-10 14:43:56 +0100

    Initial commit

---
+++ Localization.lua
@@ -0,0 +1,122 @@
+--[[
+	Fury - English Localization
+
+	By: Bhaerau
+]]--
+
+BINDING_NAME_OROGUE			= "Attack";
+BINDING_HEADER_OROGUE			= "Orogue";
+
+CLASS_DRUID_OROGUE			= "Druid";
+CLASS_HUNTER_OROGUE			= "Hunter";
+CLASS_MAGE_OROGUE				= "Mage";
+CLASS_PALADIN_OROGUE			= "Paladin";
+CLASS_PRIEST_OROGUE			= "Priest";
+CLASS_ROGUE_OROGUE			= "Rogue";
+CLASS_SHAMAN_OROGUE			= "Shaman";
+CLASS_WARLOCK_OROGUE			= "Warlock";
+CLASS_WARRIOR_OROGUE			= "Warrior";
+
+ABILITY_BATTLE_SHOUT_OROGUE		= "Battle Shout";
+ABILITY_BATTLE_STANCE_OROGUE		= "Battle Stance";
+ABILITY_BERSERKER_RAGE_OROGUE		= "Berserker Rage";
+ABILITY_BERSERKER_STANCE_OROGUE		= "Berserker Stance";
+ABILITY_BLOODRAGE_OROGUE			= "Bloodrage";
+ABILITY_BLOODTHIRST_OROGUE		= "Bloodthirst";
+--ABILITY_CHALLENGING_SHOUT_OROGUE	= "Challenging Shout";
+ABILITY_CHARGE_OROGUE			= "Charge";
+ABILITY_CLEAVE_OROGUE			= "Cleave";
+--ABILITY_CONCUSSION_BLOW_OROGUE		= "Concussion Blow";
+ABILITY_DEATH_WISH_OROGUE		= "Death Wish";
+ABILITY_DEFENSIVE_STANCE_OROGUE		= "Defensive Stance";
+ABILITY_DEMORALIZING_SHOUT_OROGUE		= "Demoralizing Shout";
+ABILITY_DISARM_OROGUE			= "Disarm";
+ABILITY_EXECUTE_OROGUE			= "Execute";
+ABILITY_HAMSTRING_OROGUE			= "Hamstring";
+ABILITY_HEROIC_STRIKE_OROGUE		= "Heroic Strike";
+ABILITY_INTERCEPT_OROGUE			= "Intercept";
+--ABILITY_INTIMIDATING_SHOUT_OROGUE	= "Intimidating Shout";
+--ABILITY_LAST_STAND_OROGUE		= "Last Stand";
+--ABILITY_MOCKING_BLOW_OROGUE		= "Mocking Blow";
+ABILITY_MORTAL_STRIKE_OROGUE		= "Mortal Strike";
+ABILITY_OVERPOWER_OROGUE			= "Overpower";
+--ABILITY_PIERCING_HOWL_OROGUE		= "Piercing Howl";
+ABILITY_PUMMEL_OROGUE			= "Pummel";
+--ABILITY_RECKLESSNESS_OROGUE		= "Recklessness";
+ABILITY_REND_OROGUE			= "Rend";
+--ABILITY_RETALIATION_OROGUE		= "Retaliation";
+--ABILITY_REVENGE_OROGUE			= "Revenge";
+ABILITY_SHIELD_BASH_OROGUE		= "Shield Bash";
+--ABILITY_SHIELD_BLOCK_OROGUE		= "Shield Block";
+ABILITY_SHIELD_SLAM_OROGUE		= "Shield Slam";
+--ABILITY_SHIELD_WALL_OROGUE		= "Shield Wall";
+--ABILITY_SLAM_OROGUE			= "Slam";
+--ABILITY_SUNDER_ARMOR_OROGUE		= "Sunder Armor";
+ABILITY_SWEEPING_STRIKES_OROGUE		= "Sweeping Strikes";
+--ABILITY_TAUNT_OROGUE			= "Taunt";
+ABILITY_THUNDER_CLAP_OROGUE		= "Thunder Clap";
+ABILITY_WHIRLWIND_OROGUE			= "Whirlwind";
+
+RACIAL_BERSERKING_OROGUE			= "Berserking";
+
+CHAT_OVERPOWER1_OROGUE			= "You attack.(.+) dodges.";
+CHAT_OVERPOWER2_OROGUE			= "Your (.+) was dodged by (.+).";
+CHAT_OVERPOWER3_OROGUE			= "Your Overpower crits (.+) for (%d+)."
+CHAT_OVERPOWER4_OROGUE			= "Your Overpower hits (.+) for (%d+)."
+CHAT_OVERPOWER5_OROGUE			= "Your Overpower missed (.+)."
+CHAT_RUNNER_OROGUE			= "%s attempts to run away in fear!"
+CHAT_CAST_OROGUE				= "(.+) begins to cast (.+).";
+CHAT_INTERRUPT1_OROGUE			= "You interrupt (.+).";
+CHAT_INTERRUPT2_OROGUE			= "Your Pummel was (.+) by (.+).";
+CHAT_INTERRUPT3_OROGUE			= "Your Shield Bash was (.+) by (.+).";
+CHAT_INTERRUPT4_OROGUE			= "Your Pummel missed (.+)."
+CHAT_INTERRUPT5_OROGUE			= "Your Shield Bash missed (.+)."
+
+CHAT_FEAR_OROGUE				= "You are afflicted by Fear.";
+CHAT_INTIMIDATING_SHOUT_OROGUE		= "You are afflicted by Intimidating Shout.";
+CHAT_PSYCHIC_SCREAM_OROGUE		= "You are afflicted by Psychic Scream.";
+CHAT_PANIC_OROGUE				= "You are afflicted by Panic.";
+CHAT_BELLOWING_ROAR_OROGUE		= "You are afflicted by Bellowing Roar.";
+CHAT_ANCIENT_DESPAIR_OROGUE		= "You are afflicted by Ancient Despair.";
+CHAT_TERRIFYING_SCREECH_OROGUE		= "You are afflicted by Terrifying Screech.";
+CHAT_HOWL_OF_TERROR_OROGUE		= "You are afflicted by Howl of Terror.";
+CHAT_SAP_OROGUE				= "You are afflicted by Sap.";
+CHAT_GOUGE_OROGUE				= "You are afflicted by Gouge.";
+CHAT_REPENTANCE_OROGUE			= "You are afflicted by Repentance.";
+CHAT_ROCKET_HELM_OROGUE			= "You are afflicted by Reckless Charge.";
+
+CHAT_FEAR2_OROGUE				= "Fear fades from you.";
+CHAT_INTIMIDATING_SHOUT2_OROGUE		= "Intimidating Shout fades from you.";
+CHAT_PSYCHIC_SCREAM2_OROGUE		= "Psychic Scream fades from you.";
+CHAT_PANIC2_OROGUE			= "Panic fades from you.";
+CHAT_BELLOWING_ROAR2_OROGUE		= "Bellowing Roar fades from you.";
+CHAT_ANCIENT_DESPAIR2_OROGUE		= "Ancient Despair fades from you.";
+CHAT_TERRIFYING_SCREECH2_OROGUE		= "Terrifying Screech fades from you.";
+CHAT_HOWL_OF_TERROR2_OROGUE		= "Howl of Terror fades from you.";
+CHAT_SAP2_OROGUE				= "Sap fades from you.";
+CHAT_GOUGE2_OROGUE			= "Gouge fades from you.";
+CHAT_REPENTANCE2_OROGUE			= "Repentance fades from you.";
+CHAT_ROCKET_HELM2_OROGUE			= "Reckless Charge fades from you.";
+
+ITEM_GAUNTLETS1_OROGUE			= "Knight-Lieutenant's Plate Gauntlets";
+ITEM_GAUNTLETS2_OROGUE			= "Marshal's Plate Gauntlets";
+ITEM_GAUNTLETS3_OROGUE			= "Blood Guard's Plate Gloves";
+ITEM_GAUNTLETS4_OROGUE			= "General's Plate Gauntlets";
+ITEM_SHIELDS_OROGUE			= "Shields";
+
+SLASH_OROGUE_ENABLED			= "enabled";
+SLASH_OROGUE_DISABLED			= "disabled";
+SLASH_OROGUE_DEFAULT			= "default";
+SLASH_OROGUE_DEBUG			= "Debug";
+SLASH_OROGUE_AUTOATTACK			= "Autoattack";
+SLASH_OROGUE_DANCE			= "Rage allowed to be wasted when switching stance set to ";
+SLASH_OROGUE_RAGE				= "Maximum rage allowed when using abilities to gain rage set to ";
+SLASH_OROGUE_RAGE				= "Minimum rage required when using Heroic Strike or Cleave set to ";
+SLASH_OROGUE_BLOODRAGE			= "Minimum percent of health required when using Bloodrage set to ";
+SLASH_OROGUE_HAMSTRING			= "Maximum percent of health allowed when using Hamstring on NPCs set to ";
+SLASH_OROGUE_LOWTHREAT			= "Low threat enabled, enabled Cleave.";
+SLASH_OROGUE_HIGHTHREAT			= "High threat enabled, enabled Heroic Strike.";
+SLASH_OROGUE_TROLL			= "Maximum percent of health allowed when using Berserking set to ";
+SLASH_OROGUE_STANCE			= "Primary stance set to ";
+SLASH_OROGUE_NOSTANCE			= "Stance switching ";
+SLASH_OROGUE_HELP				= "Help text, under construction!";
